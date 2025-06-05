a04d351912211b2a4fd346d1299f696af7cad973
Salau Kafayat <shilekhaffy@gmail.com>
2025-06-05 12:05:42 +0100

    built testimonials section

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 //mobile menu
 const hamburger = document.querySelector('#hamburger');
 const menu = document.querySelector('#menu');
-const hlink = document.querySelector('#hlink');
+const hlink = document.querySelectorAll('#hlink');
 const faSolid = document.querySelector('.fa-solid');
 
 hamburger.addEventListener('click', () => {
@@ -17,5 +17,20 @@ hamburger.addEventListener('click', () => {
 });
 
 //testimonials
-const userText = document.getElementsByClassName('user-text');
-const userPic = document.getElementsByClassName('user-pic');
+const userTexts = document.getElementsByClassName('user-text');
+const userPics = document.getElementsByClassName('user-pic');
+
+function showReview() {
+  for (userPic of userPics) {
+    userPic.classList.remove('active-pic');
+  }
+
+  for (userText of userTexts) {
+    userText.classList.remove('active-text');
+  }
+
+  let i = Array.from(userPics).indexOf(event.target);
+
+  userPics[i].classList.add('active-pic');
+  userTexts[i].classList.add('active-text');
+}
