e244b77006995eaad1422f1e67123c9a89a40861
Salau Kafayat <shilekhaffy@gmail.com>
2025-06-05 11:40:50 +0100

    created js file

---
+++ src/main.js
@@ -0,0 +1,21 @@
+//mobile menu
+const hamburger = document.querySelector('#hamburger');
+const menu = document.querySelector('#menu');
+const hlink = document.querySelector('#hlink');
+const faSolid = document.querySelector('.fa-solid');
+
+hamburger.addEventListener('click', () => {
+  menu.classList.toggle('hidden');
+  faSolid.classList.toggle('fa-xmark');
+
+  hlink.forEach((link) => {
+    link.addEventListener('click', () => {
+      menu.classList.toggle('hidden');
+      faSolid.classList.toggle('fa-xmark');
+    });
+  });
+});
+
+//testimonials
+const userText = document.getElementsByClassName('user-text');
+const userPic = document.getElementsByClassName('user-pic');
